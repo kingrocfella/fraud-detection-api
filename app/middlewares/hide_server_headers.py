169449d5f81612664161801789776e4ea1ccd661
Kingsley <kingsley.abia@weather.com>
2025-12-05 21:11:18 +0000

    fix server header bug

--- app/middlewares/hide_server_headers.py
+++ app/middlewares/hide_server_headers.py
@@ -1,6 +1,8 @@
-from fastapi import Request, Response
+from fastapi import HTTPException, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 from typing import Callable
+
+from app.config import logger
 
 
 class HideServerHeadersMiddleware(BaseHTTPMiddleware):
@@ -8,10 +10,16 @@ class HideServerHeadersMiddleware(BaseHTTPMiddleware):
 
     async def dispatch(self, request: Request, call_next: Callable) -> Response:
         """Dispatch the middleware."""
-        response = await call_next(request)
+        try:
+            response = await call_next(request)
 
-        # Remove headers
-        for header in ["server", "x-powered-by", "date"]:
-            response.headers.pop(header, None)
+            for header in ["server", "x-powered-by", "date"]:
+                if header in response.headers:
+                    del response.headers[header]
 
-        return response
+            return response
+        except Exception as e:
+            logger.error(
+                "Error in HideServerHeadersMiddleware: %s", str(e), exc_info=True
+            )
+            raise HTTPException(status_code=500, detail="Internal server error") from e
